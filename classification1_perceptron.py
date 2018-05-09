06e3e9b9b8b457ef0741fc9969a2d9ac8cefc252
위키북스 <dylee@wikibook.co.kr>
2018-05-09 10:12:57 +0900

    들여쓰기 오류 수정

--- classification1_perceptron.py
+++ classification1_perceptron.py
@@ -19,7 +19,7 @@ def f(x):
 # 반복할 횟수
 epoch = 10
 
-# 갱신할 횟수
+# 갱신 횟수
 count = 0
 
 # 웨이트를 학습한다
@@ -28,9 +28,9 @@ for _ in range(epoch):
         if f(x) != y:
             w = w + y * x
 
-            # 로그를 출력한다
-            count += 1
-            print('{}회째: w = {}'.format(count, w))
+    # 로그를 출력한다
+    count += 1
+    print('{}회째: w = {}'.format(count, w))
 
 # 그래프로 나타내서 확인한다
 x1 = np.arange(0, 500)
